f8f6bc51718c83a48012027a241e41d016aa16a1
lokyen <lokyen1225@gmail.com>
2023-11-21 23:53:30 -0500

    initial commit

---
+++ tmcdata/mooc-programming-23/part03-23_multiplication/src/multiplication.py
@@ -0,0 +1,11 @@
+# Write your solution here
+
+# Please write a program which asks the user for a positive integer number. The program then prints out a list of multiplication operations until both operands reach the number given by the user. See the examples below for details:
+
+# Sample output
+# Please type in a number: 2
+# 1 x 1 = 1
+# 1 x 2 = 2
+# 2 x 1 = 2
+# 2 x 2 = 4
+
