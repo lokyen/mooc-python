037accea463b009ace73deccce860272c71ab1a3
lokyen <lokyen1225@gmail.com>
2023-11-22 14:09:16 -0500

    initial commit of the day

--- tmcdata/mooc-programming-23/part03-23_multiplication/src/multiplication.py
+++ tmcdata/mooc-programming-23/part03-23_multiplication/src/multiplication.py
@@ -9,3 +9,12 @@
 # 2 x 1 = 2
 # 2 x 2 = 4
 
+number = int(input("Please type in a number: "))
+
+i = 1
+while i <= number:
+    j = 1
+    while j <= number:
+        print(f"{i} x {j} = {i * j}")
+        j+= 1
+    i+= 1
